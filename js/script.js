2142db54a273326f906abe126c4106923c9532fd
Vladislav Denisov <denisov.ve1997@gmail.com>
2023-10-03 13:23:28 +0400

    Tabs

---
+++ js/script.js
@@ -0,0 +1,41 @@
+"use strict";
+
+window.addEventListener("DOMContentLoaded", () => {
+  // Tabs
+
+  const tabContent = document.querySelectorAll(".tabcontent");
+  const tabHeaderItems = document.querySelector(".tabheader__items");
+  const tabHeaderItem = document.querySelectorAll(".tabheader__item");
+
+  function noneTabContent() {
+    tabContent.forEach((item) => {
+      item.style.display = "none";
+    });
+  }
+
+  function showTacContent() {
+    tabHeaderItem.forEach((item, i) => {
+      if (item.classList.contains("tabheader__item_active")) {
+        tabContent[i].style.display = "block";
+      }
+    });
+  }
+
+  noneTabContent();
+  showTacContent();
+
+  tabHeaderItems.addEventListener("click", (event) => {
+    const target = event.target;
+
+    noneTabContent();
+
+    if (target.closest(".tabheader__item")) {
+      tabHeaderItem.forEach((item) => {
+        item.classList.remove("tabheader__item_active");
+      });
+    }
+
+    target.classList.add("tabheader__item_active");
+    showTacContent();
+  });
+});
